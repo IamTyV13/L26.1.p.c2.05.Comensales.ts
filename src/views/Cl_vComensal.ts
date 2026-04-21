1029e49b7f3baa64771b4402cbd613c920bf1b14
IamTy_ <iamtyv13@gmail.com>
2026-04-21 18:36:12 -0400

    prueba

---
+++ src/views/Cl_vComensal.ts
@@ -0,0 +1,27 @@
+/* Planteamiento de Proyectos 2do corte (8 Ptos)
+
+- Objetivos que se persiguen:
+   + Aplicar intensivamente herencia y polimorfismo
+   + Diseño al detalle con UML (diagrams.com)
+   + Correspondencia 1-1/UML-TS (clases, componentes, identificadores, etc)
+   + Cada equipo desarrolla modelo único y 2 vistas (HTML-plain y Bootstrap)
+   + Plantear al menos 10 registros demostrativos, con todos los resultados dominados
+   + Estos registros se cargan al arrancar el proyecto
+   + Ambos proyectos se despliegan con VERCEL
+
+- Se tiene la clase base Persona, con los siguientes datos:
+   -> Nombre
+   -> Apellido
+   -> Cédula
+   -> Sexo (M-F)
+   -> Fecha de nacimiento (aaaa-mm-dd)
+
+-COMENSALES: Gestión de comensales
+   -> Se registran los datos del comensal
+   -> Se conoce además de cada uno:
+       + Turno de comida: 1-Desayuno, 2-Almuerzo, 3-Ambos
+	   + Costos: $5, $7, $10
+	   + Personas de 3ra edad (> 50 mujeres / >60 hombres) pagan solo 50%
+   -> Reportar totales varios
+   -> Repotar porcentajes por turno
+   -> Repotar porcentajes por descuento */
